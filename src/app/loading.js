eab1d842e96fae68a248f90093a5b39fb0b51398
AbdelrahmanElzayat <bodyelmajec @gmail.com>
2025-02-18 17:41:28 +0200

    fix bugs

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,10 +1,12 @@
 import React from "react";
+import loadingImg from "@/assets/images/loading.gif";
+import Image from "next/image";
 
 const LoadingPage = ({ message = "Loading..." }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-background-dark">
       <div className="text-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#F7941DA8] border-solid border-opacity-75"></div>
+        <Image src={loadingImg} alt="Loading" className="w-full h-full" />
         <p className="mt-4 text-lg font-medium text-gray-700 dark:text-gray-300">
           {message}
         </p>
